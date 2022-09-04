50248de86d38223096fa637b77453ee3e0ea7c33
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-04 09:30:30 +0200

    added interactive cart ui

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,33 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
+  const dispatch = useDispatch();
+
   const { title, quantity, total, price } = props.item;
+
+  const removeItemsHandler = () => {
+    dispatch(cartActions.removeItems(id));
+  };
+
+  const addItemsHandler = () => {
+    dispatch(
+      cartActions.addItems({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +36,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemsHandler}>-</button>
+          <button onClick={addItemsHandler}>+</button>
         </div>
       </div>
     </li>
